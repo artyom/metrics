32a57810376308835ddc458ef19f63143544e835
Naitik Shah <n@daaku.org>
2013-09-18 17:50:55 -0700

    mention its a fork

--- doc.go
+++ doc.go
@@ -1,5 +1,6 @@
 // Package metrics provides implementations of various measuring instruments.
 //
 // This library is based on Coda Hale's original work:
-// https://github.com/codahale/metrics.
+// https://github.com/codahale/metrics, and a fork of
+// https://github.com/rcrowley/go-metrics.
 package metrics
