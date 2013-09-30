72a43000a0abc55e26bc06963dafc3244de693da
Naitik Shah <n@daaku.org>
2013-09-30 14:19:45 -0700

    split Tickable to it's own file

--- ewma.go
+++ ewma.go
@@ -4,18 +4,7 @@ import (
 	"math"
 	"sync"
 	"sync/atomic"
-	"time"
 )
-
-// TickDuration defines the rate at which Tick() should get called for EWMA and
-// other Tickable things that rely on EWMA like Meter & Timer.
-const TickDuration = 5 * time.Second
-
-// Tickable defines the interface implemented by metrics that need to Tick.
-type Tickable interface {
-	// Tick the clock to update the moving average.
-	Tick()
-}
 
 // EWMAs continuously calculate an exponentially-weighted moving average
 // based on an outside source of clock ticks.
