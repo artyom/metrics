fa81beb625d5b7224d8fcd7c764c400b21267f5d
Artyom Pervukhin <artyom@evasive.ru>
2014-05-23 10:04:02 +0400

    Add an example of proper Tick() method call

--- tickable.go
+++ tickable.go
@@ -6,6 +6,16 @@ import (
 
 // TickDuration defines the rate at which Tick() should get called for EWMA and
 // other Tickable things that rely on EWMA like Meter & Timer.
+//
+// It is caller's responsibility to call Tick() method; the usual case is to
+// spawn a goroutine to do this:
+//
+//	e := metrics.NewEWMA1()
+//	go func(f func()){
+//		for _ = range time.Tick(metrics.TickDuration) {
+//			f()
+//		}
+//	}(e.Tick)
 const TickDuration = 5 * time.Second
 
 // Tickable defines the interface implemented by metrics that need to Tick.
